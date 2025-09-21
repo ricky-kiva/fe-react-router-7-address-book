02afd5bbbbcfdacf61dda62a933a2d049b8d97de
ricky-kiva <ricky.kiva@gmail.com>
2025-09-21 14:20:21 +0700

    feat(app): seperate `about` from `sidebar`

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,12 @@
-import { index, route, type RouteConfig } from "@react-router/dev/routes";
+import { index, layout, route, type RouteConfig } from "@react-router/dev/routes";
 
 const ROUTES_DIR = "routes"; 
+const LAYOUTS_DIR = "layouts";
 
 export default [
-  index(`${ROUTES_DIR}/home.tsx`),
-  route("contacts/:contactId", `${ROUTES_DIR}/contact.tsx`),
+  layout(`${LAYOUTS_DIR}/sidebar.tsx`, [
+    index(`${ROUTES_DIR}/home.tsx`),
+    route("contacts/:contactId", `${ROUTES_DIR}/contact.tsx`)
+  ]),
   route("about", `${ROUTES_DIR}/about.tsx`)
 ] satisfies RouteConfig;
