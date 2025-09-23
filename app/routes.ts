44f67ac6466d6d00291e67d4424635b39aaf8bc4
ricky-kiva <ricky.kiva@gmail.com>
2025-09-23 15:15:34 +0700

    feat(app): impl. `destroy` `contact` route

--- app/routes.ts
+++ app/routes.ts
@@ -7,7 +7,8 @@ export default [
   layout(`${LAYOUTS_DIR}/sidebar.tsx`, [
     index(`${ROUTES_DIR}/home.tsx`),
     route("contacts/:contactId", `${ROUTES_DIR}/contact.tsx`),
-    route("contacts/:contactId/edit", `${ROUTES_DIR}/edit-contact.tsx`)
+    route("contacts/:contactId/edit", `${ROUTES_DIR}/edit-contact.tsx`),
+    route("contacts/:contactId/destroy", `${ROUTES_DIR}/destroy-contact.tsx`)
   ]),
   route("about", `${ROUTES_DIR}/about.tsx`)
 ] satisfies RouteConfig;
