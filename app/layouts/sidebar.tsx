2d8cfdf13c532f8d6e540dffe553c4cc1c92cbcf
ricky-kiva <ricky.kiva@gmail.com>
2025-09-23 15:07:25 +0700

    feat(app): impl. `useNavigation` for nav. load anim.

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Form, Link, NavLink, Outlet } from "react-router";
+import { Form, Link, NavLink, Outlet, useNavigation } from "react-router";
 import type { Route } from './+types/sidebar';
 import { getContacts } from '../data';
 
@@ -10,6 +10,7 @@ export async function loader() {
 
 export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
   const { contacts } = loaderData;
+  const navigation = useNavigation();
 
   return (
     <>
@@ -70,7 +71,10 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
           )}
         </nav>
       </div>
-      <div id='detail'>
+      <div 
+        id='detail'
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
