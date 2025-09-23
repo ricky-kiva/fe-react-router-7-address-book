4a7cc6371940b78097e21cd590f8075d6bf0a8fd
ricky-kiva <ricky.kiva@gmail.com>
2025-09-23 15:22:39 +0700

    feat(routes): impl. `cancel` btn. on `edit-contact` route

--- app/routes/edit-contact.tsx
+++ app/routes/edit-contact.tsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router";
+import { Form, redirect, useNavigate } from "react-router";
 import { getContact, updateContact } from "../data";
 import type { Route } from './+types/edit-contact';
 
@@ -24,6 +24,7 @@ export async function action({ params, request}: Route.ActionArgs) {
 
 export default function EditContact({ loaderData }: Route.ComponentProps) {
   const { contact } = loaderData;
+  const navigate = useNavigate();
 
   return (
     <Form key={contact.id} id="contact-form" method="post">
@@ -73,7 +74,7 @@ export default function EditContact({ loaderData }: Route.ComponentProps) {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button" onClick={() => navigate(-1)}>Cancel</button>
       </p>
     </Form>
   );
